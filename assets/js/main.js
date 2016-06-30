be1c0e5460e0da3c0de2b2ce6df20dbe20533e7d
Kaleb Ganz <kjganz@gmail.com>
2016-07-01 07:57:20 +0900

    Fix canvas sizing and display range value

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,15 @@ var pixel_input    = document.getElementById('pixel-size');
 var canvas = document.getElementById('canvas');
 
 image_select.addEventListener('change', displayImage, false);
+
+// http://codepen.io/thelifemgmt/pen/CeLqA
+pixel_input.addEventListener('input', function() {
+  document.getElementById('range-value').innerHTML = this.value;
+}, false);
+
 var img;
 
+// Display the chosen image on screen
 function displayImage() {
   img = new Image();
 
@@ -16,26 +23,39 @@ function displayImage() {
 
     ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
     pixel_input.addEventListener('change', pixelateImage, false);
+    pixelateImage();
   };
 
   img.src = URL.createObjectURL(image_select.files[0]);
 }
 
+// Gather image pixel info, draw larger average color pixels
 function pixelateImage() {
   var pixel_size = parseInt(pixel_input.value, 10);
   var ctx = canvas.getContext("2d");
   ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
+  // No need to do any pixelization if pixel size is 1
   if (pixel_size <= 1) return;
 
   for (var x = 0; x < img.width; x += pixel_size) {
     for (var y = 0; y < img.height; y += pixel_size) {
 
-      // Check if pixel size would be off canvas 
-      // and clip to canvas edge to stop dark right 
-      // and bottom border from happening
-      var rgba_list = ctx.getImageData(x, y, pixel_size, pixel_size).data;
+      // Don't sample pixels if they're off the image
+      var x_pixel_offset = pixel_size;
+      var y_pixel_offset = pixel_size;
+      if (x + pixel_size >= img.width) {
+        x_pixel_offset = pixel_size - ((x + pixel_size) - img.width);
+      }
+      if (y + pixel_size >= img.height) {
+        y_pixel_offset = pixel_size - ((y + pixel_size) - img.height);
+      }
 
+      // Get the pixel colors from given area of image
+      var rgba_list = ctx.getImageData(x, y, x_pixel_offset, y_pixel_offset).data;
+
+      // Average the individual image pixel colors to
+      // make the larger pixel color
       var rgba = [0, 0, 0, 0];
       for (var i = 0; i < rgba_list.length; i++) {
         rgba[i % 4] += rgba_list[i];
@@ -45,6 +65,7 @@ function pixelateImage() {
         rgba[i] = Math.floor(rgba[i] / (rgba_list.length / 4));
       }
 
+      // Draw new larger pixel to canvas
       ctx.fillStyle = 'rgba(' +
                         rgba[0] + ',' +
                         rgba[1] + ',' +
